4798f7160eba814da8d00574da8a08d0bc3db519
siul008 <86720411+siul008@users.noreply.github.com>
2025-03-07 20:27:14 +0100

    Utils

---
+++ Utils/utils.cpp
@@ -0,0 +1,6 @@
+#include "utils.hpp"
+
+float Utils::PosIntToFloat(int pos)
+{
+    return ((pos * 0.25) + 0.125);
+}
---
+++ Utils/utils.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "gladiator.h"
+
+class Utils
+{
+    public:
+        static float PosIntToFloat(int pos);
+};
